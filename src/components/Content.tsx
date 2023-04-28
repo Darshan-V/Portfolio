d81f6e09897e1150c00d03f0add37e415c7d3d69
Darshan-V <mr.darshanv@gmail.com>
2023-04-28 11:55:46 +0530

    add nav buttons

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,10 +5,21 @@ import RandomIcon from "./RandomIcon"
 const Content = () => {
   return (
     <div className='flex flex-col w-full h-full'>
-      <div className=' w-full h-10 bg-yellow-600 rounded-md'>
+      <div className='flex flex-row-reverse w-full h-10 bg-yellow-600 rounded-md'>
         <RandomIcon />
+        <div className='flex absolute w-1/6 h-10 justify-between px-3'>
+          <button className=' font-Neucha font-thin text-xl hover:scale-125'>
+            Contact
+          </button>
+          <button className=' font-Neucha font-thin text-xl hover:scale-125'>
+            About
+          </button>
+          <button className=' font-Neucha font-thin text-xl hover:scale-125 '>
+            Skills
+          </button>
+        </div>
       </div>
-      <div className='w-full m-auto p-10'>
+      <div className='w-full p-10'>
         <Banner />
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div className='flex m-4 p-auto h-full justify-between'>
-      <div className='flex w-full h-full px-3 hover:animate-bounce'>
+    <div className='flex m-4 p-auto h-full sticky top-1'>
+      <div className='flex w-full h-full px-3 hover:animate-bounce m-auto '>
         <Link
           href='/'
           className='sm:text-md md:text-lg lg:text-xl xl-text:2xl 2xl:text-3xl font-thin italic font-Pacifico  text-yellow-500  '
