36ec07d869057801321a7707bd3fcea2a47a11f5
Darshan-V <mr.darshanv@gmail.com>
2023-06-02 18:54:47 +0530

    export home.tsx as index.tsx

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Content from "@/components/Content"
+
+const index = () => {
+  return (
+    <div className='m-2 p-2'>
+      <Content />
+    </div>
+  )
+}
+
+export default index
