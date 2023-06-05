eb9279a20581fcd1878086bfff0b59355a793888
Darshan-V <mr.darshanv@gmail.com>
2023-06-05 16:02:23 +0530

    add screen size

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import Content from "@/components/Content"
 
 const index = () => {
   return (
-    <div className='m-2 p-2'>
+    <div className='xs:w-screen sm:w-screen md:w-screen lg:w-screen xl:w-screen 2xl:w-screen'>
       <Content />
     </div>
   )
