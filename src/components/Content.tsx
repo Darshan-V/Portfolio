408b04ad00d938625522d05aece99981d70bb392
Darshan-V <mr.darshanv@gmail.com>
2023-06-27 15:44:09 +0530

    modify styles for mobile view add new div for home screen

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Banner from "./Banner"
 import MoreMe from "./MoreMe"
+import SomemoreStuff from "./SomemoreStuff"
 
 const Content = () => {
   return (
@@ -8,8 +9,9 @@ const Content = () => {
       <div className='flex w-screen p-10'>
         <Banner />
       </div>
-      <div className='hidden w-screen md:flex px-10 h-full'>
+      <div className=' flex w-screen md:flex-row flex-col px-10 h-full'>
         <MoreMe />
+        <SomemoreStuff />
       </div>
     </div>
   )
