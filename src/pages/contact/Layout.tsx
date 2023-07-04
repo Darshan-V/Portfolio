013aa43015f6754c8e37e2f1c1ea5a164b4fffc5
Darshan-V <mr.darshanv@gmail.com>
2023-07-04 20:25:43 +0530

    add hero

--- src/pages/contact/Layout.tsx
+++ src/pages/contact/Layout.tsx
@@ -4,8 +4,10 @@ const Layout = () => {
   return (
     <div className='flex w-full h-full mt-5 p-5 '>
       <div className='flex flex-col w-full h-full overflow-scroll'>
-        <div className=' w-1/2 h-fit py-2 bg-gradient-to-r from-yellow-500 m-auto rounded -skew-x-2'>
-          <h1 className='md:text-3xl text-lg font-Sriracha px-5'>Hire Me!</h1>
+        <div className=' md:w-1/2 w-full h-fit py-2 bg-gradient-to-r from-yellow-500 m-auto rounded -skew-x-2'>
+          <h1 className='md:text-3xl text-lg font-Sriracha px-5 text-black'>
+            Hire Me!
+          </h1>
         </div>
         <ContactBox />
       </div>
