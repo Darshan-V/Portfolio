92d9d468d57f2f911073dc3f1e38cd7129d589ee
Darshan-V <mr.darshanv@gmail.com>
2023-07-04 19:36:46 +0530

    create media link component for about page

---
+++ src/pages/about/MediaLink.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const MediaLink = () => {
+  return (
+    <div className='flex w-1/2'>
+      <div className='grid'></div>
+    </div>
+  )
+}
+
+export default MediaLink
