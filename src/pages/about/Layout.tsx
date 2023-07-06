6ac575c5f3f8f53afdabe30afbfe6172211852ea
Darshan-V <mr.darshanv@gmail.com>
2023-07-06 18:30:34 +0530

    add style for the layout file

--- src/pages/about/Layout.tsx
+++ src/pages/about/Layout.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Hero from "./Hero"
+import MediaLink from "./MediaLink"
 
 const Layout = () => {
   return (
-    <div>
+    <div className='flex flex-col space-y-3 h-full '>
       <Hero />
+      <MediaLink />
     </div>
   )
 }
