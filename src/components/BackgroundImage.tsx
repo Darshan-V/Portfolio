11ed19d68bdff7b5fab408d6036e90df1633ae01
Darshan-V <mr.darshanv@gmail.com>
2023-07-06 18:29:14 +0530

    add group for the hover action

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -7,14 +7,14 @@ const BackgroundImage = () => {
     <>
       <AspectRatio.Root
         ratio={16 / 6}
-        className='flex justify-center items-center rounded-[50%] hover:animate-pulse bg-gradient-to-b from-slate-700 shadow-[#8a8989] shadow-inner w-fit h-fit'
+        className='flex justify-center items-center rounded-[50%] group bg-gradient-to-b from-slate-700 shadow-[#8a8989] shadow-inner w-fit h-fit'
       >
         <Image
           src='/letter-d-final.svg'
           alt='bg'
           width='200'
           height='200'
-          className='rounded-[50%] hover:animate-none hover:animate-bounce'
+          className='rounded-[50%] hover:animate-none  group-hover:animate-[bounce_2s_ease-in-out_infinite]'
         />
       </AspectRatio.Root>
     </>
